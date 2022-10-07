c98b799584086cb0b7eafb54cd451b1a48fbc4a6
ritazakhodyaeva <57389482+ritazakhodyaeva@users.noreply.github.com>
2022-10-07 12:17:34 +0400

    Update Form1.vb

--- VB/WindowsApplication53/Form1.vb
+++ VB/WindowsApplication53/Form1.vb
@@ -3,14 +3,11 @@ Imports System.Data
 Imports System.Windows.Forms
 
 Namespace WindowsApplication53
-
     Public Partial Class Form1
         Inherits Form
-
         Public Sub New()
             InitializeComponent()
         End Sub
-
         Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs)
             PopulateTable()
             pivotGridControl1.RefreshData()
@@ -18,7 +15,6 @@ Namespace WindowsApplication53
             pivotGridControl1.BestFitDataHeaders(True)
             chartControl1.SeriesTemplate.ChangeView(DevExpress.XtraCharts.ViewType.Line)
         End Sub
-
         Private Sub PopulateTable()
             Dim myTable As DataTable = dataSet1.Tables("Data")
             myTable.Rows.Add(New Object() {"Chocolate", Date.Today, 7})
@@ -34,7 +30,6 @@ Namespace WindowsApplication53
             myTable.Rows.Add(New Object() {"Konbu", Date.Today.AddDays(2), 8})
             myTable.Rows.Add(New Object() {"Konbu", Date.Today.AddDays(4), 22})
         End Sub
-
         Private Sub pivotGridControl1_CustomChartDataSourceRows(ByVal sender As Object, ByVal e As DevExpress.XtraPivotGrid.PivotCustomChartDataSourceRowsEventArgs)
             For i As Integer = e.Rows.Count - 1 To 0 Step -1
                 If Not Equals(e.Rows(i).RowValueInfo.DataField, fieldValue) Then e.Rows.RemoveAt(i)
